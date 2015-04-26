ba2dd6ebde31a2cab49fa6b760a5ce05df563575
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2015-04-25 18:47:39 -0600

    Changed license

--- pelicanconf.py
+++ pelicanconf.py
@@ -111,7 +111,7 @@ TWITTER_USERNAME = u'kdheepak89'
 FEATURED_IMAGE = SITEURL + '/theme/images/apple-touch-icon-152x152.png'
 
 # Legal
-SITE_LICENSE = u'<div xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/" about="http://kdheepak.com/"> <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">CC BY-SA 3.0</a></div>'
+SITE_LICENSE = u'<div xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/" about="http://kdheepak.com/"> <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</div>'
 
 # SO
 SITE_DESCRIPTION = u'My name is Dheepak Krishnamurthy. This is my personal blog.'
