13b78825e4c253c5d3972c4b627e5b51bcc032cd
Dheepak Krishnamurthy <me@kdheepak.com>
2020-07-02 01:49:14 -0600

    Update files

--- scripts/links-targets-blank.lua
+++ scripts/links-targets-blank.lua
@@ -1,7 +1,7 @@
 -- Add target="_blank" attributes to all links in a Pandoc document
 
 local function add_target_blank (link)
-    if string.match(link.target, "^http") ~= true and string.match(link.target, "blog.kdheepak.com") == false then  -- here .target == href attribute
+    if string.match(link.target, "^http") ~= true and string.match(link.target, "kdheepak.com") == false then  -- here .target == href attribute
         link.attributes.target = '_blank'       -- here .target == target attribute
     end
     return link
