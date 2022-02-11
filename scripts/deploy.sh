88b3eb6e8923c6cdb21702b0a135cb5fa2713b2d
Dheepak Krishnamurthy <me@kdheepak.com>
2022-02-11 00:47:09 -0700

    chore: Update files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,5 @@ cd build
 git init
 git add -A
 git commit -m 'deploy' -n
-git push -f git@github.com:kdheepak/kdheepak.github.io.git src:gh-pages
+git push -f git@github.com:kdheepak/kdheepak.github.io.git main:gh-pages
 cd -
