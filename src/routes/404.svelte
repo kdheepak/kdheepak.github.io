20a7b6a21ff748c86450f0e403bb1f2b424aab88
Dheepak Krishnamurthy <me@kdheepak.com>
2022-02-11 00:54:21 -0700

    chore: Update files

---
+++ src/routes/404.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { base } from '$app/paths'
+</script>
+
+<svelte:head>
+  <title>404</title>
+  <link
+    rel="alternate"
+    type="application/rss+xml"
+    title="RSS"
+    href="https://blog.kdheepak.com/rss.xml"
+  />
+</svelte:head>
+
+<article>
+  <header>
+    <h1 class="title">
+      <a class="home" href="https://kdheepak.com">~</a> / 404
+    </h1>
+  </header>
+
+  <section>
+    <p>
+      Sorry, this URL is broken.
+      <a class="home" href="https://kdheepak.com">Go back to the home page</a> to find all blog
+      posts. If you would you like to report this, please open an issue
+      <a href="https://github.com/kdheepak/kdheepak.github.io/issues" target="_blank">here</a>.
+    </p>
+  </section>
+</article>
