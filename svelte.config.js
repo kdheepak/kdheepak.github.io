7a5d139db97122210acdd0c0558e198ca97d94af
Dheepak Krishnamurthy <me@kdheepak.com>
2022-04-03 00:25:47 -0600

    chore: Update files

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,9 @@ import preprocess from 'svelte-preprocess'
 import importAssets from 'svelte-preprocess-import-assets'
 
 import adapterStatic from '@sveltejs/adapter-static'
+import adapterAuto from '@sveltejs/adapter-auto'
 
-function adapter(options) {
+function _adapter(options) {
   const baseStatic = adapterStatic(options)
   const pages = options?.pages || 'build'
   return {
@@ -16,6 +17,9 @@ function adapter(options) {
 
 const pathsBase =
   process.env.PATHS_BASE === undefined ? '' : process.env.PATHS_BASE
+
+const adapter =
+  process.env.CLOUDFLARE === undefined ? _adapter : adapterAuto
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
