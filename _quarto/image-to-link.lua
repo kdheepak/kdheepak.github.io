a92cbd1ccdb268c675f159d64ca984b1470ca531
Dheepak Krishnamurthy <me@kdheepak.com>
2023-08-14 23:47:24 -0400

    chore: Update files

--- _quarto/image-to-link.lua
+++ _quarto/image-to-link.lua
@@ -1,7 +1,13 @@
--- img_to_link.lua
+-- img-to-link.lua
 function Image(img)
-  local link = pandoc.Link("", img.src, "")
-  link.content = { img }
-  link.attributes["target"] = "_blank"
-  return link
+  -- Check if the image has the attribute clickable=true
+  if img.attributes["clickable"] == "true" then
+    local link = pandoc.Link("", img.src, "")
+    link.content = { img }
+    link.attributes["target"] = "_blank"
+    return link
+  else
+    -- If the image doesn't have the attribute, return it unmodified
+    return img
+  end
 end
