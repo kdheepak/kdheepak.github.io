f84a7b58227a1c18e251d002b011afbf18c13e21
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2026-09-11 23:53:38 -0400

    refactor(config): restructure imports and update markdown processor usage

--- astro.config.ts
+++ astro.config.ts
@@ -1,25 +1,27 @@
-import { defineConfig, envField, fontProviders } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
+import { unified } from "@astrojs/markdown-remark";
 import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
-import rehypeCitation from "rehype-citation";
-import rehypeKatex from "rehype-katex";
-import rehypeRaw from "rehype-raw";
-import rehypeMermaid from "rehype-mermaid";
-import remarkDirective from "remark-directive";
-import remarkMath from "remark-math";
+import tailwindcss from "@tailwindcss/vite";
 import {
   transformerNotationDiff,
   transformerNotationHighlight,
   transformerNotationWordHighlight,
 } from "@shikijs/transformers";
-import { transformerFileName } from "./src/utils/transformers/fileName";
-import { remarkDirectives } from "./src/utils/remark-directives";
-import { remarkImageAttributes } from "./src/utils/remark-image-attributes";
-import { remarkInlineMark } from "./src/utils/remark-inline-mark";
-import { remarkMermaidFences } from "./src/utils/remark-mermaid-fences";
-import { remarkNormalizeCodeFences } from "./src/utils/remark-normalize-code-fences";
+import { defineConfig, envField, fontProviders } from "astro/config";
+import expressiveCode from "astro-expressive-code";
+import icon from "astro-icon";
+import rehypeCitation from "rehype-citation";
+import rehypeKatex from "rehype-katex";
+import rehypeMermaid from "rehype-mermaid";
+import rehypeRaw from "rehype-raw";
+import remarkDirective from "remark-directive";
+import remarkMath from "remark-math";
+
+import { SITE } from "./src/config";
+import {
+  copyBlogWwwDirs,
+  sitemapAlias,
+} from "./src/utils/astro-build-hook-integrations";
 import { rehypeExternalLinks } from "./src/utils/rehype-external-links";
 import { rehypePostEnhancements } from "./src/utils/rehype-post-enhancements";
 import {
@@ -27,20 +29,19 @@ import {
   rehypeRestoreCodeCitations,
 } from "./src/utils/rehype-protect-code-citations";
 import { rehypeSvgbob } from "./src/utils/rehype-svgbob";
+import { transformerFileName } from "./src/utils/transformers/fileName";
+import { remarkDirectives } from "./src/utils/remark-directives";
+import { remarkImageAttributes } from "./src/utils/remark-image-attributes";
+import { remarkInlineMark } from "./src/utils/remark-inline-mark";
+import { remarkMermaidFences } from "./src/utils/remark-mermaid-fences";
+import { remarkNormalizeCodeFences } from "./src/utils/remark-normalize-code-fences";
 import { remarkPostToc } from "./src/utils/remark-post-toc";
-import {
-  copyBlogWwwDirs,
-  sitemapAlias,
-} from "./src/utils/astro-build-hook-integrations";
-import { SITE } from "./src/config";
 
-import expressiveCode from "astro-expressive-code";
-
-// https://astro.build/config
 export default defineConfig({
   trailingSlash: "always",
 
   site: SITE.website,
+
   integrations: [
     expressiveCode({
       themes: ["github-light", "github-dark"],
@@ -55,44 +56,50 @@ export default defineConfig({
   ],
 
   markdown: {
-    smartypants: false,
-    remarkPlugins: [
-      remarkMath,
-      remarkNormalizeCodeFences,
-      remarkMermaidFences,
-      remarkDirective,
-      remarkInlineMark,
-      remarkDirectives,
-      remarkImageAttributes,
-      remarkPostToc,
-    ],
-    rehypePlugins: [
-      rehypeSvgbob,
-      rehypeRaw,
-      [rehypeKatex, { throwOnError: false, strict: false }],
-      rehypeProtectCodeCitations,
-      [
-        rehypeCitation,
-        {
-          path: process.cwd(),
-          bibliography: "src/data/bibliography/bibliography.bib",
-          csl: "src/data/blog/writing-papers-with-markdown/www/ieee.csl",
-          linkCitations: true,
-          showTooltips: true,
-        },
+    processor: unified({
+      smartypants: false,
+      remarkPlugins: [
+        remarkMath,
+        remarkNormalizeCodeFences,
+        remarkMermaidFences,
+        remarkDirective,
+        remarkInlineMark,
+        remarkDirectives,
+        remarkImageAttributes,
+        remarkPostToc,
       ],
-      rehypeRestoreCodeCitations,
-      [rehypeMermaid, {}],
-      [rehypeExternalLinks, { site: SITE.website }],
-      rehypePostEnhancements,
-    ],
+      rehypePlugins: [
+        rehypeSvgbob,
+        rehypeRaw,
+        [rehypeKatex, { throwOnError: false, strict: false }],
+        rehypeProtectCodeCitations,
+        [
+          rehypeCitation,
+          {
+            path: process.cwd(),
+            bibliography: "src/data/bibliography/bibliography.bib",
+            csl: "src/data/blog/writing-papers-with-markdown/www/ieee.csl",
+            linkCitations: true,
+            showTooltips: true,
+          },
+        ],
+        rehypeRestoreCodeCitations,
+        [rehypeMermaid, {}],
+        [rehypeExternalLinks, { site: SITE.website }],
+        rehypePostEnhancements,
+      ],
+    }),
+
     syntaxHighlight: {
       type: "shiki",
       excludeLangs: ["render_svgbob", "svgbob"],
     },
+
     shikiConfig: {
-      // For more themes, visit https://shiki.style/themes
-      themes: { light: "github-light", dark: "github-dark" },
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
       defaultColor: false,
       wrap: false,
       transformers: [
@@ -105,10 +112,6 @@ export default defineConfig({
   },
 
   vite: {
-    // eslint-disable-next-line
-    // @ts-ignore
-    // This will be fixed in Astro 6 with Vite 7 support
-    // See: https://github.com/withastro/astro/issues/14030
     plugins: [tailwindcss()],
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
